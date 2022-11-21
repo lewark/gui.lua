9869460faf6c1994e6b4b620d3f22f740a33e9d4
knector01 <78086132+knector01@users.noreply.github.com>
2022-11-20 23:57:47 -0700

    Use single gui.lua file in demo.lua

--- demo.lua
+++ demo.lua
@@ -1,24 +1,17 @@
-local Root = require 'gui.Root'
-local LinearContainer = require 'gui.LinearContainer'
-local Label = require 'gui.Label'
-local Button = require 'gui.Button'
-local TextField = require 'gui.TextField'
-local ListBox = require 'gui.ListBox'
-local ScrollBar = require 'gui.ScrollBar'
-local Constants = require 'gui.Constants'
+local gui = require 'gui'
 
-local root = Root()
+local root = gui.Root()
 
-local box = LinearContainer(root,Constants.LinearAxis.VERTICAL,1,1)
-local box2 = LinearContainer(root,Constants.LinearAxis.HORIZONTAL,0,0)
+local box = gui.LinearContainer(root,gui.Constants.LinearAxis.VERTICAL,1,1)
+local box2 = gui.LinearContainer(root,gui.Constants.LinearAxis.HORIZONTAL,0,0)
 
-local lbl = Label(root,"Hello!")
-local btn1 = Button(root,"Button 1")
-local btn2 = Button(root,"Button 2")
-local btn3 = Button(root,"Big Btn.")
-local field = TextField(root,5,"TextField")
-local area = ListBox(root,10,10,{})
-local sb = ScrollBar(root,area)
+local lbl = gui.Label(root,"Hello!")
+local btn1 = gui.Button(root,"Button 1")
+local btn2 = gui.Button(root,"Button 2")
+local btn3 = gui.Button(root,"Big Btn.")
+local field = gui.TextField(root,5,"TextField")
+local area = gui.ListBox(root,10,10,{})
+local sb = gui.ScrollBar(root,area)
 
 for i=1,64 do
     table.insert(area.items,"Item "..tostring(i))
@@ -36,14 +29,14 @@ end
 
 root:addChild(box2)
 
-box2:addChild(area,true,true,Constants.LinearAlign.START)
-box2:addChild(sb,false,true,Constants.LinearAlign.START)
-box2:addChild(box,false,true,Constants.LinearAlign.START)
+box2:addChild(area,true,true,gui.Constants.LinearAlign.START)
+box2:addChild(sb,false,true,gui.Constants.LinearAlign.START)
+box2:addChild(box,false,true,gui.Constants.LinearAlign.START)
 
-box:addChild(lbl,false,false,Constants.LinearAlign.START)
-box:addChild(btn1,false,false,Constants.LinearAlign.START)
-box:addChild(btn2,false,false,Constants.LinearAlign.START)
-box:addChild(field,false,true,Constants.LinearAlign.START)
-box:addChild(btn3,true,true,Constants.LinearAlign.START)
+box:addChild(lbl,false,false,gui.Constants.LinearAlign.START)
+box:addChild(btn1,false,false,gui.Constants.LinearAlign.START)
+box:addChild(btn2,false,false,gui.Constants.LinearAlign.START)
+box:addChild(field,false,true,gui.Constants.LinearAlign.START)
+box:addChild(btn3,true,true,gui.Constants.LinearAlign.START)
 
 root:mainLoop()
